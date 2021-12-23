8318c7d5ee5daa4e9918509999312f246b9590e1
moxie77 <51162433+moxie77@users.noreply.github.com>
2021-12-23 22:23:29 +0800

    Add files via upload

---
+++ Dockers/flask_api.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 23 19:26:32 2021
+
+@author: MAYOWA
+"""
+
---
+++ Dockers/flask_api_copy.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 23 21:34:14 2021
+
+@author: MAYOWA
+"""
+
